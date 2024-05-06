7b11d3814f6d7008c219552383dd4176bd7845d7
re-sha <163538829+re-sha@users.noreply.github.com>
2024-05-07 04:16:18 +0530

    Add logic for computing computer and human choice

--- scripts.js
+++ scripts.js
@@ -1,1 +1,33 @@
-console.log("Hello World")
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice (a) {
+     a = Math.floor(Math.random() * 3)+1;
+    if (a===1) {
+        console.log("rock");
+    }
+    else if (a===2) {
+        console.log("paper");
+    }
+    else {
+        console.log("scissors");
+    }
+}
+getComputerChoice();
+
+function getHumanChoice (b) {
+    let humanChoice = prompt("Enter your choice","rock | paper | scissors");
+    if (humanChoice==="rock") {
+        console.log("rock");
+    }
+    else if (humanChoice==="paper") {
+        console.log("paper");
+    }
+    else if (humanChoice==="scissors") {
+        console.log("scissors");
+    }
+    else {
+        console.log("Enter a vaild choice");
+    }
+}
+getHumanChoice();
